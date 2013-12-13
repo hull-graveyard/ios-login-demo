c37ec2b4c03d85795ddf801bed3b0ff70a5ec7d1
Jimmy Oliger <hi@jimmyoliger.com>
2013-12-13 12:19:56 +0100

    Initial Commit

---
+++ LoginDemo/HLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HLAppDelegate.h
+//  LoginDemo
+//
+//  Created by Jimmy Oliger on 13/12/2013.
+//  Copyright (c) 2013 Hull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LoginDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoginDemo
+//
+//  Created by Jimmy Oliger on 13/12/2013.
+//  Copyright (c) 2013 Hull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HLAppDelegate class]));
+    }
+}
